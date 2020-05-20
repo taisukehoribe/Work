18293d827af782354dd323a99652b8bbbf6dffe4
taisukeHORIBE0501 <taisukehoribe.14@gmail.com>
2020-05-20 12:33:24 +0900

    [start]3-9

---
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8a1bf17315340c7c9ea1259c214dda70cc29b25627a709c7d6850ab53e320298a47a433b19d1fa889efd0ead3d3fcde2a181f3b1648d578fcbdab9a8974250cf'
